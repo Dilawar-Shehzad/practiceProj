6f8d5e9fd1cf0011d7a01afbe6855ffaedcf29c8
Dilawar-Shehzad <idilawarshehzad@gmail.com>
2023-10-16 13:35:07 -0700

    update

--- App.js
+++ App.js
@@ -3,25 +3,12 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
 function App() {
-
   return (
     <SafeAreaView >
       <View style={styles.mainCont} >
